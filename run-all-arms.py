fa1711ec0c1ace451cdd158485ec6a13859a9933
MultiDream <luka.derado@gmail.com>
2020-09-05 17:08:59 -0400

    Fully completed intial translation attempt.

--- run-all-arms.py
+++ run-all-arms.py
@@ -1,19 +1,19 @@
 import numpy as np
 import pandas as pd
 
-
-
 # Todo:
 #   Table layout can be handled by a configuration file/object.
 #   Consider moving definitions related to the organization
 #   of data frames into an object to decouple this code
 #   from data organization.
 
+# Reoccuring question:
+# Are we working with np arrays or Data Frames?
 def onLoad(genotypedata_latefailures,additional_genotypedata):
     site_names = siteNames()
-    state_classification_all = np.array()    # dimensionality?
-    state_parameters_all = np.array()        # dimensionality?
-    ids_all = np.array()                    # 1-D vector
+    state_classification_all = pd.DataFrame()
+    state_parameters_all = pd.DataFrame()
+    ids_all = np.array()  # 1-D vector
 
     for site in site_names:
         # jobname = site | unecessary declaration.
@@ -34,7 +34,7 @@ def onLoad(genotypedata_latefailures,additional_genotypedata):
             str_range = np.array2string(range, separator='')
             str_range = str_range[1:len(str_range) - 1]
             str_append = 'Additional_' + str_range
-            # This is where I am beginning to see an issue.
+            # Big Problem:
             # At this point in the R script, we set the Sample element's ID to 'str_append'.
             # That doesn't work here. Why not? Because we're working with a numpy array, not a DataFrame.
             # In short, numpy array wont HAVE elements, its an array, not an obj.
@@ -43,23 +43,31 @@ def onLoad(genotypedata_latefailures,additional_genotypedata):
         (state_classification, state_parameters, ids) = runMCMC()
 
         # Not sure about concatenate vs append. Must be tested to confirm this concatenates rows of two similar tables.
-        state_classification_all = np.append(state_classification, axis=0)
-        state_parameters_all = np.append(state_parameters, axis=0)
+        state_classification_all = state_classification_all.append(state_classification, ignore_index=True)
+        state_parameters_all = state_parameters_all.append(state_parameters, ignore_index=True)
         ids_all = np.append(ids)
 
-    posterior_distribution_or_recrudescence = np.array()
-    #(?) Set the name of the first (second?) column of posterior table to 'ID'
+    # Okay, we are now going to put together the PDOR table by row binding/column binding our previous tables.
 
-    # Takes the mean of each column for every row in a 2-dim array/matrix.
-    # Behavior on empty tables needs to be evaluated.
+    # In the original script, this is a cbind2 then a rbind.
+    # I think this simpler translation accomplishes the same effect.
+    posterior_distribution_of_recrudescence = pd.DataFrame.concat([ids_all, state_classification_all])
+
+    # Set the name of the first column of PDOR table to 'ID'
+    # I don't think that's necessary, since everything gets an ID as is in Panda DataFrames,
+    # but we can add if necessary.
+
+    # Will this operation work? I read that numpy operations work over DataFrames,
+    # but I need to check this behavior to confirm.
     probability_of_recrudescence = np.mean(state_classification_all, axis=1)
 
     # Code says to generate a histogram at this point. Is that necessary? Doesn't seem like anything
     # Refrences that historgram or saves it for later viewing. If needed, use matplot package in the future.
     # Make histogram here.
 
-    #Todo: Save to CSV files using Pandas.
-
+    # Save csv's
+    posterior_distribution_of_recrudescence.write_csv('microsatellite_correction.csv', index=False)
+    probability_of_recrudescence.write_csv( "probability_of_recrudescence.csv", index=False)
     return
 
 # place holder for a global variable. Use it to wrap the logic to access a vector of site names for now.
@@ -68,9 +76,10 @@ def siteNames():
     return all_sites
 
 # runs the MCMC script.
+# are these numpy arrays, or data frames?
 def runMCMC():
-    state_classification = np.array()
-    state_parameters = np.array()
+    state_classification = pd.DataFrame()
+    state_parameters = pd.DataFrame()
     ids = np.array()
     return state_classification, state_parameters, ids
 
