d8dde68f985cb5eab078d7047d7b54ffbdef8ef4
Jake Deerin <2016deerinjo@gmail.com>
2020-09-25 15:40:33 -0400

    Initial commit of the Flask API

---
+++ api/API.md
@@ -0,0 +1,98 @@
+# Anti-Malarial Web API
+
+This is the code for the application's backend API server, which provides access to the rest of the backend's functionality.
+
+## Getting Started (Local Development)
+
+To run the code locally, install the following `pip` packages:
+
+```bash
+pip install flask
+pip install flask-restful
+```
+
+Then, you can start the server just by running `api.py`:
+
+```bash
+python api.py
+```
+
+...which should generate output like this:
+
+```
+ * Serving Flask app "api" (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: on
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 320-502-271
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+```
+
+The last line specifies the base URL that the API is running on.
+
+## User API Guide
+
+The root endpoint for all API requests for version 1.0 of the API is `/api/v1`
+
+### RecrudescenceTest
+
+Given an input `.csv` file with [drug testing data in the proper format (TODO)](), return estimates of which patients specified in the file are reinfections or recrudescences.
+
+*Method(s):* `PUSH`
+
+*URL Endpoint:* `/api/v1/recrudescences`
+
+#### PUSH
+
+**Input Parameters:** *None*
+
+-   `file` (.csv file, FormData): The file containing the testing data to be processed.
+-   (optional) `iterations` (integer): The number of iterations to run the algorithm for on the code; higher values are more accurate but take longer to complete. Defaults to 10000.
+
+**Example Usage:**
+
+In cURL:
+
+```bash
+curl -F "file=@Angola2017_example.csb" http://localhost:5000/api/v1/recrudescences?iterations=10
+
+```
+
+In Javascript (via `fetch`):
+
+```javascript
+const NUM_ITERATIONS = 100
+let formData = new FormData();
+formData.append('file', fileInputElement.files[0])
+fetch(`/api/v1/recrudescences?iterations=${NUM_ITERATIONS}`, {
+    method: 'POST',
+    body: formData
+})
+.then(response => {
+    // (...)
+});
+```
+
+**Example Response:**
+
+```json
+{
+    "runDate": "2020-09-25T18:14:44+00:00",     # ISO formatted date when request was completed
+    "totalRunTime": 1806,                       # Computation time in seconds
+    "samples": {
+        "BQ17-269": {
+            "isRecrudescence": false,
+            "probability": 0.842,
+
+        }
+    }
+}
+```
+
+**Potential Errors:**
+-   `400` - Could mean that no input file was provided, or that an invalid number of iterations as given
+-   `413` - The provided file was too large to be processed
+-   `415` - The provided file wasn't a `.csv` file
---
+++ api/api.py
@@ -0,0 +1,81 @@
+import os
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+
+# =============================================================================
+# Initialize Flask
+# =============================================================================
+
+app = Flask(__name__, template_folder='.')
+
+MAX_FILE_SIZE_MB = 50
+app.config['MAX_CONTENT_LENGTH'] = 1024**2
+app.config['UPLOAD_EXTENSIONS'] = ['.csv', '.xlsx']     # Valid filetypes
+
+api = Api(app)
+
+# =============================================================================
+# Define actual functionality
+# =============================================================================
+
+
+def error_response(message, error_code=400):
+    '''
+    Generates an appropriate JSON error message and HTML error code that can be
+    returned from a Flask API handler
+    '''
+    return {'message': message}, error_code
+
+
+@app.errorhandler(413)
+def too_large(e):
+    return error_response(f'Input file is too large (over {MAX_FILE_SIZE_MB} MB)', 413)
+
+
+class RecrudescenceTest(Resource):
+    def __init__(self):
+        # TODO: Remove!
+        self.PLACEHOLDER_RESPONSE = {
+            'runDate': '2020-09-25T18:14:44+00:00',     # ISO formatted date
+            'totalRunTime': 1806,                       # Run time in seconds
+            'samples': {
+                'BQ17-269': {
+                    'isRecrudescence': False,
+                    'probability': 0.842,
+
+                }
+            }
+        }
+
+    def post(self):
+        '''
+        Takes in a posted CSV data file, analyzes it to determine if each
+        case is a recrudescence/reinfection, and returns the calculated results
+        '''
+        uploaded_file = request.files.get('file')
+        iterations = request.args.get('iterations', default=10000, type=int)
+        # TODO: Find cleaner way of doing validation?
+        if not (uploaded_file or uploaded_file.filename):
+            return error_response('No input file provided')
+
+        file_extension = os.path.splitext(uploaded_file.filename)[-1].lower()
+        if file_extension not in app.config['UPLOAD_EXTENSIONS']:
+            return error_response('Provided file is not a .csv or .xlsx', 415)
+
+        if iterations < 0:
+            return error_response('{iterations} is an invalid iteration count')
+
+        # TODO: Actually process the file
+        return self.PLACEHOLDER_RESPONSE, 200
+
+
+# =============================================================================
+# Assign URL routes/endpoints
+# =============================================================================
+
+api.add_resource(RecrudescenceTest, '/api/v1/recrudescences')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
