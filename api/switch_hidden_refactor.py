e6a0635e0dffd7cb1e2dd61308a1f823d17230b7
MultiDream <luka.derado@gmail.com>
2020-11-21 17:29:59 -0500

    Shifted chosen and chosenLocus
    Shifted chosen and chosenLocus by -1 to account for the indexing recalculations we kept making on so many lines in the code.

--- api/switch_hidden_refactor.py
+++ api/switch_hidden_refactor.py
@@ -39,21 +39,24 @@ def switch_hidden_refactor(x, nloci, maxMOI, alleles_definitions_RR, state):
 			chosen = chosen - nloci * maxMOI
 		chosenlocus = math.ceil(chosen / maxMOI)
 
+		# this shift accounts for indexing issues we see later on.
+		chosen -= 1
+		chosenlocus -= 1
+
 		# If the sample is categorized as a reinfection...
 		if (reinfection):
 
-			old = state.recoded0[x][chosen - 1].astype(np.int64)
-			new = np.random.choice(np.arange(state.frequencies_RR[0][chosenlocus - 1])) + 1
-			oldalleles = state.recoded0[x, np.intersect1d(np.arange(((chosenlocus - 1) * maxMOI), chosenlocus * maxMOI), np.where(state.hidden0[x] == 1)[0])]
+			old = state.recoded0[x][chosen].astype(np.int64)
+			new = np.random.choice(np.arange(state.frequencies_RR[0][chosenlocus])) + 1
+			oldalleles = state.recoded0[x, np.intersect1d(np.arange((chosenlocus * maxMOI), (chosenlocus + 1) * maxMOI), np.where(state.hidden0[x] == 1)[0])]
 			repeatednew = state.qq
 
-			
+
 			if sum(oldalleles == new) >= 1:
 				repeatednew = 1
 
-			''' THIS LINE REPRESENTS WHERE I AM IN A REFACTOR PASSTHROUGH AT THE MOMENT '''
-			numerator = sum(state.frequencies_RR[1][chosenlocus - 1][0:state.frequencies_RR[0][chosenlocus - 1]] * state.dvect[state.correction_distance_matrix[chosenlocus - 1][new - 1].astype(np.int64)]) * repeatednew
-			denominator = sum(state.frequencies_RR[1][chosenlocus - 1][0:state.frequencies_RR[0][chosenlocus - 1]] * state.dvect[state.correction_distance_matrix[chosenlocus - 1][old].astype(np.int64)]) * repeatednew
+			numerator = sum(state.frequencies_RR[1][chosenlocus][0:state.frequencies_RR[0][chosenlocus]] * state.dvect[state.correction_distance_matrix[chosenlocus][new - 1].astype(np.int64)]) * repeatednew
+			denominator = sum(state.frequencies_RR[1][chosenlocus][0:state.frequencies_RR[0][chosenlocus]] * state.dvect[state.correction_distance_matrix[chosenlocus][old].astype(np.int64)]) * repeatednew
 
 			if denominator != 0:
 				alpha = numerator / denominator
@@ -62,34 +65,34 @@ def switch_hidden_refactor(x, nloci, maxMOI, alleles_definitions_RR, state):
 
 			if z < alpha:
 				if (valid_chosen):
-					state.recoded0[x][chosen - 1] = new - 1
+					state.recoded0[x][chosen] = new - 1
 				else:
-					state.recodedf[x][chosen - 1] = new - 1
-				newallele_length = (np.mean((alleles_definitions_RR[chosenlocus - 1]["0"][new-1], alleles_definitions_RR[chosenlocus - 1]["1"][new-1])) + np.random.normal(0, state.frequencies_RR[2][chosenlocus - 1], 1))[0]
-				state.alleles0[x][chosen - 1] = newallele_length
+					state.recodedf[x][chosen] = new - 1
+				newallele_length = (np.mean((alleles_definitions_RR[chosenlocus]["0"][new-1], alleles_definitions_RR[chosenlocus]["1"][new-1])) + np.random.normal(0, state.frequencies_RR[2][chosenlocus], 1))[0]
+				state.alleles0[x][chosen] = newallele_length
 
 				inputVectors = list(itertools.product(np.arange(state.MOIf[x]), np.arange(state.MOI0[x])))
 				allpossiblerecrud = pd.DataFrame(inputVectors)
 				order = [1, 0] # setting column's order
 				allpossiblerecrud = allpossiblerecrud[[allpossiblerecrud.columns[i] for i in order]]
 				allpossiblerecrud.columns = [0, 1]
-				closestrecrud = np.argmin(list(map(lambda y: abs(state.alleles0[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][y]] - state.allelesf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][y]]), np.arange(0, allpossiblerecrud.shape[0]))))
-				state.mindistance[x][chosenlocus - 1] = abs(state.alleles0[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][closestrecrud]] - state.allelesf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][closestrecrud]])
-				state.alldistance[x][chosenlocus - 1][0:allpossiblerecrud.shape[0]] = list(map(lambda y: abs(state.alleles0[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][y]] - state.allelesf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][y]]), np.arange(0, allpossiblerecrud.shape[0])))
-				state.allrecrf[x][chosenlocus - 1][0:allpossiblerecrud.shape[0]] = state.recodedf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1]]
-				state.recr0[x][chosenlocus - 1] = maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][closestrecrud]
-				state.recrf[x][chosenlocus - 1] = maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][closestrecrud]
+				closestrecrud = np.argmin(list(map(lambda y: abs(state.alleles0[x][maxMOI * chosenlocus + allpossiblerecrud[0][y]] - state.allelesf[x][maxMOI * chosenlocus + allpossiblerecrud[1][y]]), np.arange(0, allpossiblerecrud.shape[0]))))
+				state.mindistance[x][chosenlocus] = abs(state.alleles0[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][closestrecrud]] - state.allelesf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][closestrecrud]])
+				state.alldistance[x][chosenlocus][0:allpossiblerecrud.shape[0]] = list(map(lambda y: abs(state.alleles0[x][maxMOI * chosenlocus + allpossiblerecrud[0][y]] - state.allelesf[x][maxMOI * chosenlocus + allpossiblerecrud[1][y]]), np.arange(0, allpossiblerecrud.shape[0])))
+				state.allrecrf[x][chosenlocus][0:allpossiblerecrud.shape[0]] = state.recodedf[x][maxMOI * chosenlocus + allpossiblerecrud[1]]
+				state.recr0[x][chosenlocus] = maxMOI * chosenlocus + allpossiblerecrud[0][closestrecrud]
+				state.recrf[x][chosenlocus] = maxMOI * chosenlocus + allpossiblerecrud[1][closestrecrud]
 
 
 		else:
-			old = state.recoded0[x][chosen - 1].astype(np.int64)
-			new = np.random.choice(np.arange(state.frequencies_RR[0][chosenlocus - 1])) + 1
+			old = state.recoded0[x][chosen].astype(np.int64)
+			new = np.random.choice(np.arange(state.frequencies_RR[0][chosenlocus])) + 1
 			if (valid_chosen):
-				oldalleles = state.recoded0[x, np.intersect1d(np.arange(((chosenlocus - 1) * maxMOI), chosenlocus * maxMOI), np.where(state.hidden0[x] == 1)[0])]
+				oldalleles = state.recoded0[x, np.intersect1d(np.arange((chosenlocus * maxMOI), (chosenlocus + 1) * maxMOI), np.where(state.hidden0[x] == 1)[0])]
 			else:
-				oldalleles = state.recodedf[x, np.intersect1d(np.arange(((chosenlocus - 1) * maxMOI), chosenlocus * maxMOI), np.where(state.hidden0[x] == 1)[0])]
+				oldalleles = state.recodedf[x, np.intersect1d(np.arange((chosenlocus * maxMOI), (chosenlocus + 1) * maxMOI), np.where(state.hidden0[x] == 1)[0])]
 
-			newallele_length = (np.mean((alleles_definitions_RR[chosenlocus - 1]["0"][new-1], alleles_definitions_RR[chosenlocus - 1]["1"][new-1])) + np.random.normal(0, state.frequencies_RR[2][chosenlocus - 1], 1))[0]
+			newallele_length = (np.mean((alleles_definitions_RR[chosenlocus]["0"][new-1], alleles_definitions_RR[chosenlocus]["1"][new-1])) + np.random.normal(0, state.frequencies_RR[2][chosenlocus], 1))[0]
 			repeatedold = state.qq
 			repeatednew = state.qq
 
@@ -105,55 +108,55 @@ def switch_hidden_refactor(x, nloci, maxMOI, alleles_definitions_RR, state):
 			allpossiblerecrud.columns = [0, 1]
 
 			if (valid_chosen):
-				tempalleles = state.alleles0[x][maxMOI * (chosenlocus - 1): maxMOI * (chosenlocus - 1) + maxMOI]
-				tempalleles[chosen - (chosenlocus - 1) * maxMOI - 1] = newallele_length
-				temprecoded = state.recoded0[x][maxMOI * (chosenlocus - 1): maxMOI * (chosenlocus - 1) + maxMOI]
-				temprecoded[chosen - (chosenlocus - 1) * maxMOI - 1] = new - 1
+				tempalleles = state.alleles0[x][maxMOI * chosenlocus: maxMOI * chosenlocus + maxMOI]
+				tempalleles[chosen - chosenlocus * maxMOI] = newallele_length
+				temprecoded = state.recoded0[x][maxMOI * chosenlocus: maxMOI * chosenlocus + maxMOI]
+				temprecoded[chosen - chosenlocus * maxMOI] = new - 1
 
-				newclosestrecrud = np.argmin(list(map(lambda y: abs(tempalleles[allpossiblerecrud[0][y]] - state.allelesf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][y]]), np.arange(0, allpossiblerecrud.shape[0]))))
-				newmindistance = abs(tempalleles[allpossiblerecrud[0][newclosestrecrud]] - state.allelesf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][newclosestrecrud]])
-				newalldistance = list(map(lambda y: abs(tempalleles[allpossiblerecrud[0][y]] - state.allelesf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][y]]), np.arange(0, allpossiblerecrud.shape[0])))
-				newallrecrf = state.recodedf[x][maxMOI * (chosenlocus - 1) + allpossiblerecrud[1]]
+				newclosestrecrud = np.argmin(list(map(lambda y: abs(tempalleles[allpossiblerecrud[0][y]] - state.allelesf[x][maxMOI * chosenlocus + allpossiblerecrud[1][y]]), np.arange(0, allpossiblerecrud.shape[0]))))
+				newmindistance = abs(tempalleles[allpossiblerecrud[0][newclosestrecrud]] - state.allelesf[x][maxMOI * chosenlocus + allpossiblerecrud[1][newclosestrecrud]])
+				newalldistance = list(map(lambda y: abs(tempalleles[allpossiblerecrud[0][y]] - state.allelesf[x][maxMOI * chosenlocus + allpossiblerecrud[1][y]]), np.arange(0, allpossiblerecrud.shape[0])))
+				newallrecrf = state.recodedf[x][maxMOI * chosenlocus + allpossiblerecrud[1]]
 			else:
-				tempalleles = state.allelesf[x][maxMOI * (chosenlocus - 1): maxMOI * (chosenlocus - 1) + maxMOI]
-				tempalleles[chosen - (chosenlocus - 1) * maxMOI - 1] = newallele_length
-				temprecoded = state.recodedf[x][maxMOI * (chosenlocus - 1): maxMOI * (chosenlocus - 1) + maxMOI]
-				temprecoded[chosen - (chosenlocus - 1) * maxMOI - 1] = new - 1
+				tempalleles = state.allelesf[x][maxMOI * chosenlocus: maxMOI * chosenlocus + maxMOI]
+				tempalleles[chosen - chosenlocus * maxMOI] = newallele_length
+				temprecoded = state.recodedf[x][maxMOI * chosenlocus: maxMOI * chosenlocus + maxMOI]
+				temprecoded[chosen - chosenlocus * maxMOI] = new - 1
 
 				newclosestrecrud = np.argmin(list(map(lambda y: abs(
 					tempalleles[allpossiblerecrud[1][y]] - state.alleles0[x][
-						maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][y]]),
+						maxMOI * chosenlocus + allpossiblerecrud[0][y]]),
 													  np.arange(0, allpossiblerecrud.shape[0]))))
 				newmindistance = abs(tempalleles[allpossiblerecrud[1][newclosestrecrud]] - state.alleles0[x][
-					maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][newclosestrecrud]])
+					maxMOI * chosenlocus + allpossiblerecrud[0][newclosestrecrud]])
 				newalldistance = list(map(lambda y: abs(tempalleles[allpossiblerecrud[1][y]] - state.alleles0[x][
-					maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][y]]), np.arange(0, allpossiblerecrud.shape[0])))
+					maxMOI * chosenlocus + allpossiblerecrud[0][y]]), np.arange(0, allpossiblerecrud.shape[0])))
 				newallrecrf = temprecoded[allpossiblerecrud[1]]
 
-			newrecr0 = maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][newclosestrecrud]
-			newrecrf = maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][newclosestrecrud]
+			newrecr0 = maxMOI * chosenlocus + allpossiblerecrud[0][newclosestrecrud]
+			newrecrf = maxMOI * chosenlocus + allpossiblerecrud[1][newclosestrecrud]
 
 
 			## likelihoodnew
 			likelihoodnew_numerator = state.dvect[np.round(newalldistance).astype(np.int64)]
-			likelihoodnew_demominator = sum(state.frequencies_RR[1][chosenlocus - 1][0:state.frequencies_RR[0][chosenlocus - 1]] * state.dvect[state.correction_distance_matrix[chosenlocus-1][newallrecrf[0].astype(np.int64)].astype(np.int64)])
+			likelihoodnew_demominator = sum(state.frequencies_RR[1][chosenlocus][0:state.frequencies_RR[0][chosenlocus]] * state.dvect[state.correction_distance_matrix[chosenlocus][newallrecrf[0].astype(np.int64)].astype(np.int64)])
 			likelihoodnew = np.nanmean(likelihoodnew_numerator / likelihoodnew_demominator) * repeatednew
 
 
 			## likelihoodold
-			temp = np.round(state.alldistance[x][chosenlocus - 1])
+			temp = np.round(state.alldistance[x][chosenlocus])
 			temp = temp[np.logical_not(np.isnan(temp))].astype(np.int64)
 			likelihoodold_numerator = state.dvect[temp]
 
 			### NEED TO REVISIT
 			temp_allrecrf = []
 			for i in range(maxMOI * maxMOI):
-				item = state.allrecrf[x][chosenlocus-1][i]
+				item = state.allrecrf[x][chosenlocus][i]
 				if not np.isnan(item):
 					temp_allrecrf.append(item)
 			temp_allrecrf = np.asarray(temp_allrecrf).astype(np.int64)
 
-			temp = state.frequencies_RR[1][chosenlocus - 1][0:state.frequencies_RR[0][chosenlocus - 1]] * state.dvect[state.correction_distance_matrix[chosenlocus - 1][temp_allrecrf].astype(np.int64)]
+			temp = state.frequencies_RR[1][chosenlocus][0:state.frequencies_RR[0][chosenlocus]] * state.dvect[state.correction_distance_matrix[chosenlocus][temp_allrecrf].astype(np.int64)]
 			likelihoodold_denominator = []
 			for i in temp:
 				likelihoodold_denominator.append(sum(i))
@@ -171,13 +174,13 @@ def switch_hidden_refactor(x, nloci, maxMOI, alleles_definitions_RR, state):
 					alpha = 0
 
 			if z < alpha:
-				state.recoded0[x][chosen - 1] = new - 1
+				state.recoded0[x][chosen] = new - 1
 				if (valid_chosen):
-					state.alleles0[x][chosen - 1] = newallele_length
+					state.alleles0[x][chosen] = newallele_length
 				else:
-					state.allelesf[x][chosen - 1] = newallele_length
-				state.mindistance[x][chosenlocus - 1] = newmindistance
-				state.alldistance[x][chosenlocus - 1][0:allpossiblerecrud.shape[0]] = newalldistance
-				state.allrecrf[x][chosenlocus - 1][0:allpossiblerecrud.shape[0]] = newallrecrf
-				state.recr0[x][chosenlocus - 1] = maxMOI * (chosenlocus - 1) + allpossiblerecrud[0][newclosestrecrud]
-				state.recrf[x][chosenlocus - 1] = maxMOI * (chosenlocus - 1) + allpossiblerecrud[1][newclosestrecrud]
+					state.allelesf[x][chosen] = newallele_length
+				state.mindistance[x][chosenlocus] = newmindistance
+				state.alldistance[x][chosenlocus][0:allpossiblerecrud.shape[0]] = newalldistance
+				state.allrecrf[x][chosenlocus][0:allpossiblerecrud.shape[0]] = newallrecrf
+				state.recr0[x][chosenlocus] = maxMOI * (chosenlocus) + allpossiblerecrud[0][newclosestrecrud]
+				state.recrf[x][chosenlocus] = maxMOI * (chosenlocus) + allpossiblerecrud[1][newclosestrecrud]
