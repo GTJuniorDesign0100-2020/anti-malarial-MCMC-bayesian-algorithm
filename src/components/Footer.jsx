43a70502f6ab9f0b6484b9a688b00b44a849b801
Jake Deerin <2016deerinjo@gmail.com>
2020-12-05 11:09:59 -0500

    Fixed React warning about security risks when linking to new tabs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
   render() {
       return (
           <div className="footer">
-            <p>To read about the algorithm powering this application, check out its <a target='_blank' href='https://github.com/GTJuniorDesign0100-2020/anti-malarial-MCMC-bayesian-algorithm'>article</a> in the American Society for Microbiology.  If you want to contribute to this project or examine the technology, the software is available on our <a target='_blank' href='https://github.com/GTJuniorDesign0100-2020/anti-malarial-MCMC-bayesian-algorithm'>GitHub</a>.</p>
+            <p>To read about the algorithm powering this application, check out its <a target="_blank" rel="noopener noreferrer" href="https://github.com/GTJuniorDesign0100-2020/anti-malarial-MCMC-bayesian-algorithm">article</a> in the American Society for Microbiology.  If you want to contribute to this project or examine the technology, the software is available on our <a target="_blank" rel="noopener noreferrer" href="https://github.com/GTJuniorDesign0100-2020/anti-malarial-MCMC-bayesian-algorithm">GitHub</a>.</p>
           </div>
       );
   }
