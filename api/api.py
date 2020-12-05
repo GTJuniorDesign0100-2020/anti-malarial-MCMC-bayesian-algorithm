20edd56d1918a56472179942917a0fe5510ae62d
Jake Deerin <2016deerinjo@gmail.com>
2020-12-05 17:17:58 -0500

    Fixed bug w/ claiming port in Heroku

--- api/api.py
+++ api/api.py
@@ -167,4 +167,4 @@ def index():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, port=5000)
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 5000), threaded=True)
